86c7f0b796369922fe5d2f4e26842e3d800607af
mugdad <128133561+mugdad1@users.noreply.github.com>
2025-01-21 14:44:21 +0300

    .

--- first/1.cpp
+++ first/1.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
-int main(){
+#include <cmath>
+#include <string>
+using namespace std;
 
+int main() {
+string name;
+cout<<"enter the name"<<endl;
+getline(cin , name);
+if(name.empty()){
+    cout<<" name must be smaller"<<endl;
 
-std::cout<<"hi"<<std::endl;
-std::cout<<"gff"<<std::endl;
+}else{
+    cout<<"welcome"<<endl;
 }
+}
+
