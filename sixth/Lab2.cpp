825f6a68476d03647f5c66bc8b82195c4bb3f3f5
mugdad1 <alhammad5057@gmail.com>
2025-03-17 12:08:55 +0300

    hi

--- sixth/Lab2.cpp
+++ sixth/Lab2.cpp
@@ -1,3 +1,5 @@
 #include <iostream>
-using namespace std;
-int main() { return 0; }
+int main() {
+  std::cout << "hi";
+  return 0;
+}
--- third/task2.cpp
+++ third/task2.cpp
@@ -1,36 +1,34 @@
 /*
 Task: Convert the following program to implement dynamic memory allocation.
 */
-#include <iostream>
+
 #include <cstdlib>
-#include <ctime>
-using namespace std; 
-  
-int largest(int arr[], int n) 
-{ 
-    int i; 
-   
-    int max = arr[0]; 
-    
-    for (i = 1; i < n; i++) 
-        if (arr[i] > max) 
-            max = arr[i]; 
-    
-    return max; 
-} 
+#include <iostream>
+using std::cout;
+using std::srand;
+
+int largest(int arr[], int n) {
+  int i;
+
+  int max = arr[0];
+
+  for (i = 1; i < n; i++)
+    if (arr[i] > max)
+      max = arr[i];
+
+  return max;
+}
 
 int main() 
 { 
 	int N = 100;
     int arr[N];
-  	
-	srand((unsigned) time(0));
-  	for(int ctr = 0; ctr<N; ctr++)
-	{
-		 arr[ctr] = rand();
-		 cout << arr[ctr] << " ";
-		 
-	}
+
+    srand((unsigned)time(0));
+    for (int ctr = 0; ctr < N; ctr++) {
+      arr[ctr] = std::rand();
+      cout << arr[ctr] << " ";
+        }
   
     cout << "\n\nLargest value in the given array is " << largest(arr, N); 
     	 
